67bf3c9575050610a8462fe3f4541966003a8944
Steven Stanfield <stanfield@scarecrowtech.com>
2024-07-19 14:03:14 -0400

    Adjust local start params, back to dev and discovery off to avoid spamming eth main nodes. (#26)
    Co-authored-by: Steven Stanfield <steve@telcoin.org>

--- etc/local-testnet.sh
+++ etc/local-testnet.sh
@@ -87,6 +87,8 @@ for ((i=0; i<$LENGTH; i++)); do
         # start validator
         target/${RELEASE}/telcoin-network node --datadir "${DATADIR}" \
            --chain adiri \
+           --dev \
+           --disable-discovery \
            --instance "${INSTANCE}" \
            --metrics "${METRICS}" \
            --consensus-metrics "${CONSENSUS_METRICS}" \
