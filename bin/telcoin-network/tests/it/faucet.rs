40787c0d07cd0957bd79f7441deadf0c79f837d8
robriks <marsterlund@gmail.com>
2024-10-31 01:41:32 -0400

    make clippy

--- bin/telcoin-network/tests/it/faucet.rs
+++ bin/telcoin-network/tests/it/faucet.rs
@@ -213,6 +213,7 @@ async fn test_faucet_transfers_tel_and_xyz_with_google_kms_e2e() -> eyre::Result
         pre_genesis_chain.clone(),
         gas_price,
         None,
+        None,
         U256::ZERO,
         faucet_create_data.clone().into(),
     );
@@ -221,6 +222,7 @@ async fn test_faucet_transfers_tel_and_xyz_with_google_kms_e2e() -> eyre::Result
         pre_genesis_chain.clone(),
         gas_price,
         None,
+        None,
         U256::ZERO,
         stablecoin_create_data.clone().into(),
     );
@@ -228,6 +230,7 @@ async fn test_faucet_transfers_tel_and_xyz_with_google_kms_e2e() -> eyre::Result
     let role_tx_raw = tx_factory.create_eip1559(
         pre_genesis_chain.clone(),
         gas_price,
+        None,
         Some(faucet_proxy_address),
         U256::ZERO,
         grant_role_call,
@@ -373,6 +376,7 @@ async fn test_faucet_transfers_tel_and_xyz_with_google_kms_e2e() -> eyre::Result
     let tx = random_tx_factory.create_eip1559(
         chain,
         1_000_000_000,
+        None,
         Some(Address::random()),
         U256::from_str("0xaffffffffffffff").expect("U256 from str for tx factory"),
         Default::default(),
