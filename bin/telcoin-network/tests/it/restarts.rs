21e3acdcd16181c0a1772971d837a4c0480ba57c
robriks <marsterlund@gmail.com>
2025-06-15 14:01:38 -0400

    make clippy

--- bin/telcoin-network/tests/it/restarts.rs
+++ bin/telcoin-network/tests/it/restarts.rs
@@ -81,12 +81,11 @@ fn send_and_confirm(
     }
     let bal =
         get_balance_above_with_retry(node_test, &basefee_address.to_string(), current_basefee)?;
-    if nonce > 0 {
-        if bal != current_basefee + (current_basefee / (nonce)) {
+    if nonce > 0
+        && bal != current_basefee + (current_basefee / (nonce)) {
             error!(target: "restart-test", "basefee error!");
-            return Err(Report::msg(format!("Expected a basefee increment!")));
+            return Err(Report::msg("Expected a basefee increment!".to_string()));
         }
-    }
     Ok(())
 }
 
